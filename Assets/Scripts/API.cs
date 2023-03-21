db7332cf49e92982d48706c848e32d4bb14a5eb8
AnuragDubey <AnuragDubeyGames@gmail.com>
2023-03-21 21:03:56 +0530

    Done Mileseton 2

--- Assets/Scripts/API.cs
+++ Assets/Scripts/API.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -8,20 +9,60 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class API : MonoBehaviour
 {
     
-    const string BundleFolder = "https://stl-loader-adarsh.s3.amazonaws.com/Models/";
+    const string ModelFolder = "https://stl-loader-adarsh.s3.amazonaws.com/Models/";
     public Material bonesMaterial;
     public string layerName = "Grabable";
+    public UI_button uiButtonTemplate;
+    public Transform UI_Holder_CONTENT;
     public List<GameObject> UI_Buttons = new List<GameObject>();
 
+    private void Start()
+    {
+        StartCoroutine(GetListOfObjects(ModelFolder));
+    }
     public void GetBundleObject(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
     {
         StartCoroutine(GetDisplayBundleRoutine(assetName, callback, bundleParent));
     }
+    private IEnumerator GetListOfObjects(string url)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            while(!request.isDone)
+            {
+                yield return null;
+            }
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Models Request Success");
+                print("Data Received : "+ request.downloadHandler.text);
+                string[] files = request.downloadHandler.text.Split('\n');
+                foreach (string file in files)
+                {
+                    Debug.Log("File: " + file);
+                }
+               /* string[] models = request.downloadHandler.text.Split('\n');
 
+                foreach (string model in models)
+                {
+                    GameObject newUI = Instantiate(uiButtonTemplate.gameObject);
+                    newUI.GetComponent<UI_button>().ModelName = model;
+                    newUI.transform.parent = UI_Holder_CONTENT.transform;
+                    newUI.transform.position = Vector3.zero;
+                    UI_Buttons.Add(newUI);
+                }*/
+            }
+            else
+            {
+                Debug.Log("Models Request Failed: " + request.error);
+            }
+        }
+    }
     IEnumerator GetDisplayBundleRoutine(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
     {
 
-        string bundleURL = BundleFolder + assetName;
+        string bundleURL = ModelFolder + assetName;
 
         Debug.Log("Requesting bundle at " + bundleURL);
 
