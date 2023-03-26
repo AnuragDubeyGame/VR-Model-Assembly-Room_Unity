6297373e34850e5e0b320b9f96aaf3ae5bf0845a
AnuragDubey <AnuragDubeyGames@gmail.com>
2023-03-26 16:49:27 +0530

    I guess working S3 list fetching

--- Assets/Scripts/API.cs
+++ Assets/Scripts/API.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
 using UnityEngine.XR.Interaction.Toolkit;
-/*using Amazon.S3;
+using Amazon.S3;
 using Amazon.S3.Model;
-using Amazon.Runtime;*/
+using Amazon.Runtime;
+using Amazon;
+using UnityEngine.InputSystem;
+
 
 public class API : MonoBehaviour
 {
     const string ModelFolder = "https://stl-loader-adarsh.s3.amazonaws.com/Models/";
-    private const string bucketName = "stl-loader-adarsh";
- /*   private AmazonS3Client s3Client;*/
+    private string bucketName = "stl-loader-adarsh";
+    private string objectKey = "talus-1";
+
 
     public Material bonesMaterial;
     public string layerName = "Grabable";
@@ -21,21 +26,110 @@ public class API : MonoBehaviour
     public List<GameObject> UI_Buttons = new List<GameObject>();
 
 
-    public void GetBundleObject(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
+    private async void Start()
     {
-        StartCoroutine(GetDisplayBundleRoutine(assetName, callback, bundleParent));
+        UnityInitializer.AttachToGameObject(this.gameObject);
+        await GetListOfObjectsAsync();
+    }
+
+    public async Task GetListOfObjectsAsync()
+    {
+        AmazonS3Client S3Client = new AmazonS3Client(new AnonymousAWSCredentials(), RegionEndpoint.USEast1);
+        var listRequest = new ListObjectsRequest
+        {
+            BucketName = bucketName
+        };
+        S3Client.ListObjectsAsync(listRequest, (response) => { 
+            if(response.Exception != null)
+            {
+                Debug.Log("Exception : "+response.Exception);
+            }
+            else
+            {
+                foreach (var s3Object in response.Response.S3Objects)
+                {
+                    Debug.Log(s3Object);
+                }
+            }
+        });
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* foreach (string model in models)
-   {
-       GameObject newUI = Instantiate(uiButtonTemplate.gameObject);
-       newUI.GetComponent<UI_button>().ModelName = model;
-       newUI.transform.parent = UI_Holder_CONTENT.transform;
-       newUI.transform.position = Vector3.zero;
-       UI_Buttons.Add(newUI);
-}*/
-IEnumerator GetDisplayBundleRoutine(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
+{
+GameObject newUI = Instantiate(uiButtonTemplate.gameObject);
+newUI.GetComponent<UI_button>().ModelName = model;
+newUI.transform.parent = UI_Holder_CONTENT.transform;
+newUI.transform.position = Vector3.zero;
+UI_Buttons.Add(newUI);
+}.
+
+IEnumerator LoadGLB()
+{
+// Download the GLB file from the remote server
+using (WWW www = new WWW(glbUrl))
+{
+yield return www;
+if (!string.IsNullOrEmpty(www.error))
+{
+Debug.LogError($"Failed to load GLB file from {glbUrl}: {www.error}");
+yield break;
+}
+
+// Parse the GLB file using the GLTF package
+GLTFRoot gltfRoot = null;
+GLTFParser.ParseJson(Encoding.ASCII.GetString(www.bytes), out gltfRoot);
+
+// Create a new GameObject and add a GLTFComponent to it
+GameObject modelObject = new GameObject("GLB Model");
+GLTFComponent gltfComponent = modelObject.AddComponent<GLTFComponent>();
+gltfComponent.GLTFRoot = gltfRoot;
+gltfComponent.FileName = glbUrl;
+
+// Load the GLB model using the GLTFComponent
+yield return gltfComponent.Load();
+}
+}
+*/
+public void GetBundleObject(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
+    {
+        StartCoroutine(GetDisplayBundleRoutine(assetName, callback, bundleParent));
+    }
+    IEnumerator GetDisplayBundleRoutine(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
     {
 
         string bundleURL = ModelFolder + assetName;
