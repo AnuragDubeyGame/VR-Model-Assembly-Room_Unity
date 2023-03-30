15729a84b83f3d0f35c2f39f35ffc852f7e9abef
AnuragDubey <AnuragDubeyGames@gmail.com>
2023-03-30 14:10:27 +0530

    AWS SDK GetListObj's Fixed

--- Assets/Scripts/API.cs
+++ Assets/Scripts/API.cs
@@ -9,15 +9,14 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.Runtime;
 using Amazon;
-using UnityEngine.InputSystem;
 
 
 public class API : MonoBehaviour
 {
-    const string ModelFolder = "https://stl-loader-adarsh.s3.amazonaws.com/Models/";
     private string bucketName = "stl-loader-adarsh";
-    private string objectKey = "talus-1";
-
+    [SerializeField] public string accessKey;
+    [SerializeField] public string secretKey;
+    const string BaseModelURL = "https://stl-loader-adarsh.s3.amazonaws.com/";
 
     public Material bonesMaterial;
     public string layerName = "Grabable";
@@ -29,110 +28,75 @@ public class API : MonoBehaviour
     private async void Start()
     {
         UnityInitializer.AttachToGameObject(this.gameObject);
+        UI_Buttons.Clear();
         await GetListOfObjectsAsync();
     }
-
     public async Task GetListOfObjectsAsync()
     {
-        AmazonS3Client S3Client = new AmazonS3Client(new AnonymousAWSCredentials(), RegionEndpoint.USEast1);
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey,secretKey);
+        AmazonS3Client S3Client = new AmazonS3Client(credentials, RegionEndpoint.USEast1);
         var listRequest = new ListObjectsRequest
         {
             BucketName = bucketName
         };
-        S3Client.ListObjectsAsync(listRequest, (response) => { 
-            if(response.Exception != null)
+        S3Client.ListObjectsAsync(listRequest, (response) =>
+        {
+            if (response.Exception != null)
             {
-                Debug.Log("Exception : "+response.Exception);
+                Debug.Log("Exception : " + response.Exception);
             }
             else
             {
+                Debug.Log(response.Response.S3Objects.Count);
                 foreach (var s3Object in response.Response.S3Objects)
                 {
-                    Debug.Log(s3Object);
+                    Debug.Log(s3Object.Key);
+                    GameObject newUI = Instantiate(uiButtonTemplate.gameObject);
+                    newUI.GetComponent<UI_button>().ModelName = s3Object.Key;
+                    newUI.transform.parent = UI_Holder_CONTENT.transform;
+                    newUI.transform.localPosition = Vector3.zero;
+                    newUI.transform.localScale = Vector3.one;
+                    UI_Buttons.Add(newUI);
+                    newUI.GetComponent<UI_button>().DisplayName();
                 }
             }
         });
     }
 
+    /*IEnumerator LoadGLB()
+    {
+        // Download the GLB file from the remote server
+        using (WWW www = new WWW(glbUrl))
+        {
+            yield return www;
+            if (!string.IsNullOrEmpty(www.error))
+            {
+            Debug.LogError($"Failed to load GLB file from {glbUrl}: {www.error}");
+            yield break;
+            }
 
+            // Parse the GLB file using the GLTF package
+            GLTFRoot gltfRoot = null;
+            GLTFParser.ParseJson(Encoding.ASCII.GetString(www.bytes), out gltfRoot);
 
+            // Create a new GameObject and add a GLTFComponent to it
+            GameObject modelObject = new GameObject("GLB Model");
+            GLTFComponent gltfComponent = modelObject.AddComponent<GLTFComponent>();
+            gltfComponent.GLTFRoot = gltfRoot;
+            gltfComponent.FileName = glbUrl;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* foreach (string model in models)
-{
-GameObject newUI = Instantiate(uiButtonTemplate.gameObject);
-newUI.GetComponent<UI_button>().ModelName = model;
-newUI.transform.parent = UI_Holder_CONTENT.transform;
-newUI.transform.position = Vector3.zero;
-UI_Buttons.Add(newUI);
-}.
-
-IEnumerator LoadGLB()
-{
-// Download the GLB file from the remote server
-using (WWW www = new WWW(glbUrl))
-{
-yield return www;
-if (!string.IsNullOrEmpty(www.error))
-{
-Debug.LogError($"Failed to load GLB file from {glbUrl}: {www.error}");
-yield break;
-}
-
-// Parse the GLB file using the GLTF package
-GLTFRoot gltfRoot = null;
-GLTFParser.ParseJson(Encoding.ASCII.GetString(www.bytes), out gltfRoot);
-
-// Create a new GameObject and add a GLTFComponent to it
-GameObject modelObject = new GameObject("GLB Model");
-GLTFComponent gltfComponent = modelObject.AddComponent<GLTFComponent>();
-gltfComponent.GLTFRoot = gltfRoot;
-gltfComponent.FileName = glbUrl;
-
-// Load the GLB model using the GLTFComponent
-yield return gltfComponent.Load();
-}
-}
-*/
-public void GetBundleObject(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
+            // Load the GLB model using the GLTFComponent
+            yield return gltfComponent.Load();
+        }
+    }*/
+    
+    public void GetBundleObject(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
     {
         StartCoroutine(GetDisplayBundleRoutine(assetName, callback, bundleParent));
     }
     IEnumerator GetDisplayBundleRoutine(string assetName, UnityAction<GameObject> callback, Transform bundleParent)
     {
-
-        string bundleURL = ModelFolder + assetName;
+        string bundleURL = BaseModelURL + assetName;
 
         UnityEngine.Debug.Log("Requesting bundle at " + bundleURL);
 
@@ -140,9 +104,9 @@ public void GetBundleObject(string assetName, UnityAction<GameObject> callback, 
         UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle(bundleURL);
         yield return www.SendWebRequest();
 
-        if (www.isNetworkError)
+        if (www.result == UnityWebRequest.Result.ConnectionError)
         {
-            UnityEngine.Debug.Log("Network error");
+            Debug.Log("Network error");
         }
         else
         {
--- Assets/Scripts/UI_button.cs
+++ Assets/Scripts/UI_button.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class UI_button : MonoBehaviour
 {
     public string ModelName;
+    public TextMeshProUGUI ObjName;
+    public Button DownloadButton;
+    public AWS_ObjectLoader objLoader;
+
+    private void Start()
+    {
+        objLoader = FindObjectOfType<AWS_ObjectLoader>();
+    }
+
+    public void DisplayName()
+    {
+        ObjName.text = ModelName;
+        DownloadButton.onClick.AddListener(delegate { objLoader.LoadContent(ModelName); } );
+    }
 }
